8b6b844ec9978358f0f21225706a239f87ad5eb2
sun_xiaoMei <18640402163@163.com>
2019-10-11 16:46:33 +0800

    chat style

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -66,6 +66,7 @@ export default {
   height: 50px;
   overflow: hidden;
   position: relative;
+  border-bottom: 1px solid;
   background: #fff;
   box-shadow: 0 1px 4px rgba(0, 21, 41, 0.08);
   .svg-container {
